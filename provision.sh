736cff9f3501cfa60fb2b0b65fd945438756ba6a
Igor Sarcevic <igisar@gmail.com>
2016-12-17 23:55:13 +0100

    Install vim

--- provision.sh
+++ provision.sh
@@ -7,13 +7,16 @@ sudo apt-get install -qq -y figlet > /dev/null
 figlet "Box Box"
 
 echo "[PROVISIONER] Installing Basic Tools"
-sudo apt-get install -qq -y git tmux zsh vim curl wget firefox build-essential > /dev/null
+sudo apt-get install -qq -y git tmux zsh curl wget firefox build-essential > /dev/null
 
 echo "[PROVISIONER] Changing shell to zsh"
 sudo chsh -s /bin/zsh
 
 echo "[PROVISIONER] Installing Postgres"
 sudo apt-get install -qq -y postgresql postgresql-contrib > /dev/null
+
+echo "[PROVISIONER] Installing Vim"
+bash /vagrant/scripts/install_vim.sh
 
 echo "[PROVISIONER] Installing Docker"
 bash /vagrant/scripts/install_docker.sh
