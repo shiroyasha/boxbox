1ad3fe446a832cf88aa162a5d2b5254b2468e829
bmarkons <mamaveb@gmail.com>
2017-11-22 10:52:16 +0100

    Create developer postgres superuser

--- provision.sh
+++ provision.sh
@@ -38,6 +38,9 @@ sudo apt-get install -y postgresql-client-9.4 postgresql-9.4 postgresql-contrib-
 sudo update-rc.d postgresql enable
 sudo service postgresql start
 
+echo "[PROVISIONER] Creating postgres superuser 'developer'"
+createuser developer --superuser --password developer
+
 echo "[PROVISIONER] Installing rabbitmq"
 echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
 wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
